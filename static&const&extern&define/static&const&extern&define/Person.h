6173289cfdf070644552f9611b356033ff489026
dzr <804426132@qq.com>
2022-09-02 12:07:29 +0800

    feat:OC内存分区

---
+++ static&const&extern&define/static&const&extern&define/Person.h
@@ -0,0 +1,16 @@
+//
+//  Person.h
+//  static&const&extern&define
+//
+//  Created by tp on 2022/9/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ static&const&extern&define/static&const&extern&define/Person.m
@@ -0,0 +1,12 @@
+//
+//  Person.m
+//  static&const&extern&define
+//
+//  Created by tp on 2022/9/2.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
